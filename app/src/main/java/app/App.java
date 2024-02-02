bc906262390f693aed1baaa13b95938d86691bab
KirillTheARCHIK <kirillthearchik@yandex.ru>
2024-02-02 19:57:38 +0400

    1

--- app/src/main/java/app/App.java
+++ app/src/main/java/app/App.java
@@ -1,18 +1,7 @@
 package app;
 
-import java.util.Scanner;
-
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input a number: ");
-        int num = in.nextInt();
-          
-        System.out.printf("Your number: %d \n", num);
-        in.close();
+        
     }
 }
