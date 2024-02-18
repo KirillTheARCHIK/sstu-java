9654223db65590497f992d8bd38e3d9e6802f363
KirillTheARCHIK <kirillthearchik@yandex.ru>
2024-02-18 12:41:55 +0400

    1

--- app/src/main/java/app/Sum.java
+++ app/src/main/java/app/Sum.java
@@ -2,10 +2,15 @@ package app;
 
 import app.utils.Utils;
 
-public class App {
+public class Sum {
     public static void main(String[] args) {
         Utils.clearConsole();
         System.out.println("\n----------------------------PROGRAM STARTED-----------------------------\n");
+        int S = 0;
+        for (String arg : args) {
+            S += Integer.parseInt(arg);
+        }
+        System.out.println("Sum = " + S);
         System.out.println("\n----------------------------PROGRAM FINISHED----------------------------\n");
     }
 }
