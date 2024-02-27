87408913189a759d2e52c5cebee46d73949a829e
KirillTheARCHIK <kirillthearchik@yandex.ru>
2024-02-27 21:08:35 +0400

    1

--- app/src/main/java/app/Sum.java
+++ app/src/main/java/app/Sum.java
@@ -1,16 +1,21 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import app.utils.Utils;
 
 public class Sum {
     public static void main(String[] args) {
         Utils.clearConsole();
         System.out.println("\n----------------------------PROGRAM STARTED-----------------------------\n");
-        int S = 0;
-        for (String arg : args) {
-            S += Integer.parseInt(arg);
-        }
-        System.out.println("Sum = " + S);
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list.toString());
         System.out.println("\n----------------------------PROGRAM FINISHED----------------------------\n");
     }
 }
--- app/src/main/java/app/utils/Utils.java
+++ app/src/main/java/app/utils/Utils.java
@@ -1,7 +1,6 @@
 package app.utils;
 
 public class Utils {
-
   public final static void clearConsole() {
     try {
       String operatingSystem = System.getProperty("os.name"); // Check the current operating system
@@ -20,4 +19,31 @@ public class Utils {
       System.out.println(e);
     }
   }
+
+  public static int[] concat(int[] a1, int[] a2) {
+    int newArrLength = a1.length + a2.length;
+    int[] newArr = new int[newArrLength];
+    for (int i = 0; i < newArrLength; i++) {
+      if (i < a1.length) {
+        newArr[i] = a1[i];
+      } else {
+        newArr[i] = a2[i - a1.length];
+      }
+    }
+    return newArr;
+  }
+
+  public static String toString(int[] arr) {
+    String str = "[";
+    for (int i = 0; i < arr.length; i++) {
+      str += arr[i];
+      if (i < arr.length - 1) {
+        str += ", ";
+      }
+    }
+    str += "]";
+    return str;
+  }
+
+  public static final double PI = 3.14;
 }
