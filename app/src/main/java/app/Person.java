662f66eeffa50be4c342ee1763d536db9626e28b
KirillTheARCHIK <kirillthearchik@yandex.ru>
2024-03-03 16:18:41 +0400

    1

---
+++ app/src/main/java/app/Person.java
@@ -0,0 +1,16 @@
+package app;
+
+public class Person {
+  String name;
+  int height;
+
+  public Person(String name, int height) {
+    this.name = name;
+    this.height = height;
+  }
+
+  @Override
+  public String toString() {
+    return name + ", рост: " + height;
+  }
+}
