0f8335b6f2aa85d69b81671256f20778aa447ac1
KirillTheARCHIK <kirillthearchik@yandex.ru>
2024-03-04 14:02:46 +0400

    1

--- app/src/main/java/app/PolyLine.java
+++ app/src/main/java/app/PolyLine.java
@@ -5,6 +5,10 @@ import java.util.*;
 public class PolyLine {
   ArrayList<Point> points;
 
+  public PolyLine() {
+    this.points = new ArrayList<Point>();
+  }
+
   public PolyLine(ArrayList<Point> points) {
     this.points = points;
   }
