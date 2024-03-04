07a41e4bb16434c89649fa15e29119833080e05f
KirillTheARCHIK <kirillthearchik@yandex.ru>
2024-03-04 14:44:15 +0400

    1

--- app/src/main/java/app/App.java
+++ app/src/main/java/app/App.java
@@ -6,6 +6,9 @@ public class App {
     public static void main(String[] args) {
         Utils.clearConsole();
         System.out.println("\n----------------------------PROGRAM STARTED-----------------------------\n");
+        var f1 = new Fraction(1, 3);
+        var f2 = new Fraction(2, 4);
+        System.out.println(f1 + " * " + f2 + " = " + f1.sum(f2));
         System.out.println("\n----------------------------PROGRAM FINISHED----------------------------\n");
     }
 }
--- app/src/main/java/app/Fraction.java
+++ app/src/main/java/app/Fraction.java
@@ -4,8 +4,34 @@ public class Fraction {
   int numerator;
   int denominator;
 
+  Fraction(int numerator) {
+    this.numerator = numerator;
+    this.denominator = 1;
+  }
+
   Fraction(int numerator, int denominator) {
     this.numerator = numerator;
     this.denominator = denominator;
+  }
+
+  @Override
+  public String toString() {
+    return numerator + "/" + denominator;
+  }
+
+  Fraction sum(Fraction other) {
+    return new Fraction(numerator * other.denominator + other.numerator * denominator, denominator * other.denominator);
+  }
+
+  Fraction sub(Fraction other) {
+    return sum(new Fraction(-other.numerator, other.denominator));
+  }
+
+  Fraction mul(Fraction other) {
+    return new Fraction(numerator * other.numerator, denominator * other.denominator);
+  }
+
+  Fraction div(Fraction other) {
+    return mul(new Fraction(other.denominator, other.numerator));
   }
 }
