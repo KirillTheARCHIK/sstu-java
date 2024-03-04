0da666c0350cee4a3a7740a8eb1f280e7a1f3cd0
KirillTheARCHIK <kirillthearchik@yandex.ru>
2024-03-04 14:27:01 +0400

    1

--- app/src/main/java/app/Line.java
+++ app/src/main/java/app/Line.java
@@ -10,8 +10,7 @@ public class Line {
   }
 
   public Line(int x1, int y1, int x2, int y2) {
-    this.start = new Point(x1, y1);
-    this.end = new Point(x2, y2);
+    this(new Point(x1, y1),new Point(x2, y2));
   }
 
   @Override
