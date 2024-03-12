d140e833ffc6ce5807187f8a0fa83bbe3f0f8c91
KirillTheARCHIK <kirillthearchik@yandex.ru>
2024-03-12 21:07:29 +0400

    1

--- app/src/main/java/app/App.java
+++ app/src/main/java/app/App.java
@@ -3,13 +3,20 @@ package app;
 import app.utils.Utils;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Utils.clearConsole();
         System.out.println("\n----------------------------PROGRAM STARTED-----------------------------\n");
-        var gun = new Gun(3);
+        var gun = new Gun(7);
+        gun.addAmmo(3);
         for (int i = 0; i < 5; i++) {
             gun.Fire();
         }
+        gun.addAmmo(8);
+        for (int i = 0; i < 2; i++) {
+            gun.Fire();
+        }
+        gun.setAmmo(0);
+        gun.Fire();
         System.out.println("\n----------------------------PROGRAM FINISHED----------------------------\n");
     }
 }
--- app/src/main/java/app/Gun.java
+++ app/src/main/java/app/Gun.java
@@ -1,14 +1,39 @@
 package app;
 
 public class Gun {
-  int ammo;
+  private int ammo;
+  private int capacity;
 
-  public Gun() {
-    ammo = 5;
+  public Gun(int capacity) {
+    setCapacity(capacity);
+    setAmmo(0);
   }
 
-  public Gun(int ammo) {
+  public int getAmmo() {
+    return ammo;
+  }
+
+  public void setAmmo(int ammo) {
     this.ammo = ammo;
+  }
+
+  public int getCapacity() {
+    return capacity;
+  }
+
+  void setCapacity(int capacity) {
+    this.capacity = capacity;
+  }
+
+  public boolean isLoaded() {
+    return ammo > 0;
+  }
+
+  public void addAmmo(int ammo) throws Exception {
+    if (ammo < 0) {
+      throw new Exception("Negative ammo");
+    }
+    setAmmo(getAmmo() + ammo > capacity - getAmmo() ? capacity - getAmmo() : ammo);
   }
 
   public void Fire() {
