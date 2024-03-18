bf3dd953036f2654f7d79f8f8ca04836074d1da0
KirillTheARCHIK <kirillthearchik@yandex.ru>
2024-03-18 14:43:16 +0400

    1

--- app/src/main/java/app/Square.java
+++ app/src/main/java/app/Square.java
@@ -1,49 +1,11 @@
 package app;
 
-import java.util.*;;
-
-public class Square extends Figure {
-  Point leftTopCorner;
-  int size;
-
-  public Square(Point leftTopCorner, int size) throws Exception {
-    setLeftTopCorner(leftTopCorner);
-    setSize(size);
+public class Square extends Rectangle {
+  public Square(Point leftTopCorner, int size) {
+    super(leftTopCorner, size, size);
   }
 
-  public Square(int x, int y, int size) throws Exception {
+  public Square(int x, int y, int size) {
     this(new Point(x, y), size);
-  }
-
-  @Override
-  public String toString() {
-    return "Квадрат в точке " + leftTopCorner + " со стороной " + size;
-  }
-
-  public PolyLine getPolyLine() {
-    return new PolyLine(new ArrayList<>(List.of(
-        leftTopCorner,
-        new Point(leftTopCorner.x + size, leftTopCorner.y),
-        new Point(leftTopCorner.x + size, leftTopCorner.y + size),
-        new Point(leftTopCorner.x, leftTopCorner.y + size))));
-  }
-
-  public Point getLeftTopCorner() {
-    return leftTopCorner;
-  }
-
-  public void setLeftTopCorner(Point leftTopCorner) {
-    this.leftTopCorner = leftTopCorner;
-  }
-
-  public int getSize() {
-    return size;
-  }
-
-  public void setSize(int size) throws Exception {
-    if (size <= 0) {
-      throw new Exception();
-    }
-    this.size = size;
   }
 }
