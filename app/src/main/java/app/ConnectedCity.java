c4361684207bab86d90cc2e08cd477e901a74122
KirillTheARCHIK <kirillthearchik@yandex.ru>
2024-03-27 09:20:19 +0400

    1

--- app/src/main/java/app/ConnectedCity.java
+++ app/src/main/java/app/ConnectedCity.java
@@ -8,6 +8,9 @@ public class ConnectedCity extends City {
   @Override
   public void addCityLink(CityLink cityLink) {
     super.addCityLink(cityLink);
+    if (cityLink.to.getCityLinks().stream().anyMatch(cl -> cl.to == this)) {
+      return;
+    }
     cityLink.to.addCityLink(new CityLink(this, cityLink.cost));
   }
 }
