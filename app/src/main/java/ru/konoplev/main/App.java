961ffa02970e7feef88e388d0f7d19cbb49c9181
KirillTheARCHIK <kirillthearchik@yandex.ru>
2024-04-15 14:03:53 +0400

    1

--- app/src/main/java/ru/konoplev/main/App.java
+++ app/src/main/java/ru/konoplev/main/App.java
@@ -3,7 +3,7 @@ package ru.konoplev.main;
 import ru.konoplev.utils.Utils;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Utils.clearConsole();
         System.out.println("\n----------------------------PROGRAM STARTED-----------------------------\n");
         System.out.println("\n----------------------------PROGRAM FINISHED----------------------------\n");
